4c79ad5d0560fcaa84998a80ef01f7f8c5e05eff
sibiniiii <101381931+sibiniiii@users.noreply.github.com>
2024-06-17 17:21:28 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,23 @@
-resource "aws_vpc" "main_vpc" {
-  cidr_block           = "172.0.0.0/16"
-  enable_dns_hostnames = true
-  enable_dns_support   = true
-  tags = {
-    Name = "sb-test-vpc"
-  }
+#resource "aws_vpc" "main_vpc" {
+#  cidr_block           = "172.0.0.0/16"
+#  enable_dns_hostnames = true
+#  enable_dns_support   = true
+#  tags = {
+#    Name = "sb-test-vpc"
+#  }
+#}
+
+module "vpc" {
+  source  = "tfe.ddimtech.click/ddimtech/vpc/aws"
+  version = "1.0.0"
+
+  vpc_cidr_block       = var.vpc_cidr_block
+  enable_dns_hostnames = var.enable_dns_hostnames
+  enable_dns_support   = var.enable_dns_support
+  vpc_tag              = var.vpc_tag
+  tags                 = local.tags
+
 }
+
+
+
