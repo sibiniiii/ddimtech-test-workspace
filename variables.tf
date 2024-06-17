5cc1700ddd49f2e35a005c8fe89a2a0e12c119d8
sibiniiii <101381931+sibiniiii@users.noreply.github.com>
2024-06-17 17:21:46 +0900

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,3 +5,26 @@ variable "access_key" {
 variable "secret_key" {
   type    = string
 }
+
+################################################################################
+# VPC
+################################################################################
+variable "vpc_cidr_block" {
+  type    = string
+  default = "10.10.0.0/16"
+}
+
+variable "enable_dns_hostnames" {
+  type    = string
+  default = "true"
+}
+
+variable "enable_dns_support" {
+  type    = string
+  default = "true"
+}
+
+variable "vpc_tag" {
+  type    = string
+  default = "sb_main_vpc"
+}
