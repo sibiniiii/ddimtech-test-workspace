a440a435d357bfa89e5372a8a4165da499128f6e
sibiniiii <101381931+sibiniiii@users.noreply.github.com>
2024-06-20 13:17:25 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,5 +19,46 @@ module "vpc" {
 
 }
 
+resource "aws_subnet" "public1" {
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = "172.0.0.0/24"
+  availability_zone       = "ap-northeast-2a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "sb-public1-subnet"
+  }
+}
+
+resource "aws_subnet" "public2" {
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = "172.0.1.0/24"
+  availability_zone = "ap-northeast-2c"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "sb-public2-subnet"
+  }
+}
+
+resource "aws_subnet" "private1" {
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = "172.0.2.0/24"
+  availability_zone = "ap-northeast-2a"
+
+  tags = {
+    Name = "sb-private1-subnet"
+  }
+}
+
+resource "aws_subnet" "private2" {
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = "172.0.3.0/24"
+  availability_zone = "ap-northeast-2c"
+
+  tags = {
+    Name = "sb-private2-subnet"
+  }
+}
 
 
